8fa14e5c3edcb58dca05526139679346ab1e85e9
bashow <bashow0316@gmail.com>
2020-07-18 22:31:26 +0900

    Add install_nodejs.sh

---
+++ linux/install_nodejs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+### INSTALL nodejs Script
+
+cd ~/sindan-client/linux
+mkdir -p trace-json
+sudo apt install chromium-browser
+sudo apt install npm
+npm i puppeteer-core
+npm i speedline
+
