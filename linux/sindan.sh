e08e1628f7c484fbbae7bf8c7437ce8c38c1cdaf
Yoshiaki KITAGUCHI <kitaguchi@gsic.titech.ac.jp>
2020-07-28 20:15:12 +0900

    Changed measurement group from IPv46 to Dualstack.

--- linux/sindan.sh
+++ linux/sindan.sh
@@ -1539,12 +1539,11 @@ cmdset_speedindex() {
   fi
   local layer=$1
   local ver=$2
-  local ipv=IPv${ver}
   local type=$3
   local target=$4
   local count=$5
   local result=$FAIL
-  local string=" speedindex to extarnal server: $target by $ipv"
+  local string=" speedindex to extarnal server: $target by $ver"
   local speedindex_ans
 
   if speedindex_ans=$(do_speedindex ${target}); then
@@ -1552,7 +1551,7 @@ cmdset_speedindex() {
   else
     stat=$?
   fi
-  write_json "$layer" "$ipv" "v${ver}speedindex" "$result" "$target"	\
+  write_json "$layer" "$ver" speedindex "$result" "$target"	\
              "$speedindex_ans" "$count"
   if [ "$result" = "$SUCCESS" ]; then
     string="$string\n  status: ok, speed index value: $speedindex_ans"
@@ -1640,12 +1639,11 @@ cmdset_speedtest() {
   fi
   local layer=$1
   local ver=$2
-  local ipv=IPv${ver}
   local type=$3
   local target=$4
   local count=$5
   local result=$FAIL
-  local string=" speedtest to extarnal server: $target by $ipv"
+  local string=" speedtest to extarnal server: $target by $ver"
   local speedtest_ans
   local ipv4_rtt; local ipv4_jit; local ipv4_dl; local ipv4_ul
   local ipv6_rtt; local ipv6_jit; local ipv6_dl; local ipv6_ul
@@ -1657,43 +1655,45 @@ cmdset_speedtest() {
   fi
   if [ "$result" = "$SUCCESS" ]; then
     string="$string\n  status: ok"
+    write_json "$layer" "$ver" speedtest "$result" "$target"	\
+               "$speedtest_ans" "$count"
     if ipv4_rtt=$(echo "$speedtest_ans" | get_speedtest_ipv4_rtt); then
-      write_json "$layer" "ipv4" "v4speedtest_rtt" "$result" "$target"	\
+      write_json "$layer" IPv4 v4speedtest_rtt "$INFO" "$target"	\
                  "$ipv4_rtt" "$count"
       string="$string\n  IPv4 RTT: $ipv4_rtt ms"
     fi
     if ipv4_jit=$(echo "$speedtest_ans" | get_speedtest_ipv4_jit); then
-      write_json "$layer" "ipv4" "v4speedtest_jit" "$result" "$target"	\
+      write_json "$layer" IPv4 v4speedtest_jitter "$INFO" "$target"	\
                  "$ipv4_jit" "$count"
       string="$string\n  IPv4 Jitter: $ipv4_jit ms"
     fi
     if ipv4_dl=$(echo "$speedtest_ans" | get_speedtest_ipv4_dl); then
-      write_json "$layer" "ipv4" "v4speedtest_dl" "$result" "$target"	\
+      write_json "$layer" IPv4 v4speedtest_download "$INFO" "$target"	\
                  "$ipv4_dl" "$count"
       string="$string\n  IPv4 Download Speed: $ipv4_dl Mbps"
     fi
     if ipv4_ul=$(echo "$speedtest_ans" | get_speedtest_ipv4_ul); then
-      write_json "$layer" "ipv4" "v4speedtest_ul" "$result" "$target"	\
+      write_json "$layer" IPv4 v4speedtest_upload "$INFO" "$target"	\
                  "$ipv4_ul" "$count"
       string="$string\n  IPv4 Upload Speed: $ipv4_ul Mbps"
     fi
     if ipv6_rtt=$(echo "$speedtest_ans" | get_speedtest_ipv6_rtt); then
-      write_json "$layer" "ipv6" "v6speedtest_rtt" "$result" "$target"	\
+      write_json "$layer" IPv6 v6speedtest_rtt "$INFO" "$target"	\
                  "$ipv6_rtt" "$count"
       string="$string\n  IPv6 RTT: $ipv6_rtt ms"
     fi
     if ipv6_jit=$(echo "$speedtest_ans" | get_speedtest_ipv6_jit); then
-      write_json "$layer" "ipv6" "v6speedtest_jit" "$result" "$target"	\
+      write_json "$layer" IPv6 v6speedtest_jitter "$INFO" "$target"	\
                  "$ipv6_jit" "$count"
       string="$string\n  IPv6 Jitter: $ipv6_jit ms"
     fi
     if ipv6_dl=$(echo "$speedtest_ans" | get_speedtest_ipv6_dl); then
-      write_json "$layer" "ipv6" "v6speedtest_dl" "$result" "$target"	\
+      write_json "$layer" IPv6 v6speedtest_download "$INFO" "$target"	\
                  "$ipv6_dl" "$count"
       string="$string\n  IPv6 Download Speed: $ipv6_dl Mbps"
     fi
     if ipv6_ul=$(echo "$speedtest_ans" | get_speedtest_ipv6_ul); then
-      write_json "$layer" "ipv6" "v6speedtest_ul" "$result" "$target"	\
+      write_json "$layer" IPv6 v6speedtest_upload "$INFO" "$target"	\
                  "$ipv6_ul" "$count"
       string="$string\n  IPv6 Upload Speed: $ipv6_ul Mbps"
     fi
@@ -2591,7 +2591,7 @@ if [ "$v4addr_type" = "private" ] || [ "$v4addr_type" = "grobal" ] ||	\
     for target in $(echo "$SI_SRVS" | sed 's/,/ /g'); do
 
       # Do speedindex
-      cmdset_speedindex "$layer" 46 speedidsrv "$target" "$count"
+      cmdset_speedindex "$layer" Dualstack speedidsrv "$target" "$count"
 
       count=$(( count + 1 ))
     done
@@ -2604,7 +2604,7 @@ if [ "$v4addr_type" = "private" ] || [ "$v4addr_type" = "grobal" ] ||	\
     for target in $(echo "$ST_SRVS" | sed 's/,/ /g'); do
 
       # Do speedtest
-      cmdset_speedtest "$layer" 46 speedtssrv "$target" "$count"
+      cmdset_speedtest "$layer" Dualstack speedtssrv "$target" "$count"
 
       count=$(( count + 1 ))
     done
