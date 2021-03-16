044b609c74a3071e3e0aec0b18ab242a85f4a239
Yoshiaki KITAGUCHI <kitaguchi@gsic.titech.ac.jp>
2021-03-16 10:22:07 +0900

    Changed output messages.

--- linux/sendlog.sh
+++ linux/sendlog.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 # sendlog.sh
-# version 1.3
+# version 1.4
 
 # read configurationfile
 cd $(dirname $0)
 . ./sindan.conf
 
 # Check LOCKFILE_SENDLOG parameter
-if [ -z "${LOCKFILE_SENDLOG}" ]; then
+if [ -z "$LOCKFILE_SENDLOG" ]; then
   echo "ERROR: LOCKFILE_SENDLOG is null at configration file." 1>&2
   exit 1
 fi
-trap 'rm -f ${LOCKFILE_SENDLOG}; exit 0' INT
+trap 'rm -f $LOCKFILE_SENDLOG; exit 0' INT
 
-if [ ! -e ${LOCKFILE_SENDLOG} ]; then
-  echo $$ >"${LOCKFILE_SENDLOG}"
+if [ ! -e $LOCKFILE_SENDLOG ]; then
+  echo $$ >"$LOCKFILE_SENDLOG"
 else
-  pid=`cat "${LOCKFILE_SENDLOG}"`
-  kill -0 "${pid}" > /dev/null 2>&1
+  pid=`cat "$LOCKFILE_SENDLOG"`
+  kill -0 "$pid" > /dev/null 2>&1
   if [ $? = 0 ]; then
     exit 0
   else
-    echo $$ >"${LOCKFILE_SENDLOG}"
+    echo $$ >"$LOCKFILE_SENDLOG"
     echo "Warning: previous check appears to have not finished correctly"
   fi
 fi
@@ -33,34 +33,34 @@ fi
 
 # upload campaign log
 for file in `find log/ -name "campaign_*.json"`; do
-  if [ "${VERBOSE}" = "yes" ]; then
-    echo " send campaign log to ${URL_CAMPAIGN}"
+  if [ "$VERBOSE" = "yes" ]; then
+    echo " send $file to $URL_CAMPAIGN"
   fi
-  status=`curl --max-time 5 -s -w %{http_code} -F json=@${file} ${URL_CAMPAIGN}`
-  if [ "${VERBOSE}" = "yes" ]; then
-    echo " status:${status}"
+  status=`curl --max-time 5 -s -w %{http_code} -F json=@$file $URL_CAMPAIGN`
+  if [ "$VERBOSE" = "yes" ]; then
+    echo " status:$status"
   fi
-  if [ "${status}" = "200" ]; then
-    rm -f ${file}
+  if [ "$status" = "200" ]; then
+    rm -f $file
   fi
 done
 
 # upload sindan log
 for file in `find log/ -name "sindan_*.json"`; do
-  if [ "${VERBOSE}" = "yes" ]; then
-    echo " send sindan log to ${URL_SINDAN}"
+  if [ "$VERBOSE" = "yes" ]; then
+    echo " send $file to $URL_SINDAN"
   fi
-  status=`curl --max-time 15 -s -w %{http_code} -F json=@${file} ${URL_SINDAN}`
-  if [ "${VERBOSE}" = "yes" ]; then
-    echo " status:${status}"
+  status=`curl --max-time 15 -s -w %{http_code} -F json=@$file $URL_SINDAN`
+  if [ "$VERBOSE" = "yes" ]; then
+    echo " status:$status"
   fi
-  if [ "${status}" = "200" ]; then
-    rm -f ${file}
+  if [ "$status" = "200" ]; then
+    rm -f $file
   fi
 done
 
 # remove lock file
-rm -f ${LOCKFILE_SENDLOG}
+rm -f $LOCKFILE_SENDLOG
 
 exit 0
 
